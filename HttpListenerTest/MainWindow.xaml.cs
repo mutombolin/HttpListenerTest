eefa80fb9b8a89b26d2a4325de2e0a650819fed5
mutombolin <mutombolin@gmail.com>
2013-12-31 16:20:18 +0800

    Add a new project named HttpListenerTest (C#)

---
+++ HttpListenerTest/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Threading;
+
+namespace HttpListenerTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Timers.Timer _clientTimer;
+        private System.Timers.Timer _serverTimer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _clientTimer = new System.Timers.Timer();
+            _clientTimer.Interval = 3000;
+            _clientTimer.Elapsed += new System.Timers.ElapsedEventHandler(_clientTimer_Elapsed);
+
+            Thread startThread = new Thread(new ThreadStart(StartThread));
+            startThread.Name = "StartThread";
+            startThread.Start();
+
+            _serverTimer = new System.Timers.Timer();
+            _serverTimer.Interval = 1000;
+            _serverTimer.Elapsed += new System.Timers.ElapsedEventHandler(_serverTimer_Elapsed);
+
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void _serverTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new System.Timers.ElapsedEventHandler(SAFE_serverTimer_Elapsed), sender, e);
+        }
+
+        void SAFE_serverTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _serverTimer.Stop();
+
+            Server server = new Server();
+            server.Start();
+        }
+
+        void _clientTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new System.Timers.ElapsedEventHandler(SAFE_clientTimer_Elapsed), sender, e);
+        }
+
+        void SAFE_clientTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _clientTimer.Stop();
+
+            Client client = new Client();
+            client.ConnectToServer();
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+//            _clientTimer.Start();
+//            _serverTimer.Start();
+        }
+
+        private void StartThread()
+        {
+            Server server = new Server();
+            server.Start();
+        }
+    }
+}
---
+++ HttpListenerTest/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+
+namespace HttpListenerTest
+{ 
+    public class Server
+    {
+        private HttpListener _listener;
+
+        public Server()
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://localhost:7896/");
+            _listener.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+        }
+
+        public void Start()
+        {
+            if (!HttpListener.IsSupported)
+                System.Diagnostics.Debug.WriteLine("Not supported");
+
+            _listener.Start();
+
+            while (true)
+            {
+                ProcessRequest();
+            }
+        }
+
+        private void ProcessRequest()
+        {
+            var startNew = Stopwatch.StartNew();
+            var result = _listener.BeginGetContext(ListenerCallback, _listener);
+            result.AsyncWaitHandle.WaitOne();
+            startNew.Stop();
+        }
+
+        private void ListenerCallback(IAsyncResult result)
+        {
+            var context = _listener.EndGetContext(result);
+            Thread.Sleep(1000);
+            context.Response.StatusCode = 200;
+            context.Response.StatusDescription = "OK";
+            var receivedText = context.Request.Headers["thread"] + " Received";
+            context.Response.Headers["thread"] = receivedText;
+            context.Response.Close();
+        }
+    }
+}
